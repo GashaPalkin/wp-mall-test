db5e706a209f5698dfad06bcc3adf85e7cccef0b
GashaPalkin <pavelgalkin.mgn@gmail.com>
2024-03-17 19:35:09 +0500

    добавлено возможность редактирования контента в админке WP

--- wp-content/themes/mall/functions.php
+++ wp-content/themes/mall/functions.php
@@ -1,22 +1,70 @@
 <?
 
-add_theme_support('title-tag');
-add_theme_support(
-	'post-formats',
-	array(
-		'link',
-		'aside',
-		'gallery',
-		'image',
-		'quote',
-		'status',
-		'video',
-		'audio',
-		'chat',
-	)
-);
-add_theme_support('post-thumbnails');
-set_post_thumbnail_size(1568, 9999);
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+add_action('carbon_fields_register_fields', 'main_page');
+function main_page()
+{
+
+	Container::make('post_meta', __('Контент для главной страницы'))
+		->where('post_type', '=', 'page')
+		->add_tab(
+			__('Первый экран'),
+			array(
+				Field::make('complex', 'main_slider', 'Слайдер')
+					->add_fields(
+						array(
+							Field::make('image', 'slider_photo', 'Изображение'),
+						)
+					),
+				Field::make('text', 'first_screen_title', 'Заголовок'),
+				Field::make('textarea', 'first_screen_text', 'Текст под заголовком'),
+				Field::make('text', 'first_screen_title_2', 'Второй заголовок'),
+				Field::make('rich_text', 'first_screen_rich_text', __('Текст')),
+				Field::make('textarea', 'first_screen_text_2', 'Текст под списком'),
+				Field::make('file', 'crb_file', __('Файл для скачивания'))
+			)
+		)
+		->add_tab(
+			__('Арендаторы'),
+			array(
+				Field::make('complex', 'slider_clients', 'Слайдер арендаторов')
+					->add_fields(
+						array(
+							Field::make('image', 'slider_photo', 'Изображение'),
+						)
+					),
+				Field::make('text', 'clients_title', 'Заголовок'),
+				Field::make('rich_text', 'clients_rich_text', __('Текст')),
+			)
+		)
+		->add_tab(
+			__('Баннеры'),
+			array(
+				Field::make('complex', 'numbers', 'Цифры')
+					->add_fields(
+						array(
+							Field::make('text', 'numbers_text', 'Текст'),
+							Field::make('text', 'numbers_number', 'Цифра'),
+						)
+					),
+				Field::make('image', 'banner_1', 'Баннер_1'),
+				Field::make('image', 'banner_2', 'Баннер_2'),
+
+			)
+		)
+		->add_tab(
+			__('Галерея'),
+			array(
+				Field::make('image', 'banner_3', 'Баннер_3'),
+				Field::make('image', 'banner_4', 'Баннер_4'),
+				Field::make('image', 'banner_5', 'Баннер_5'),
+			)
+		);
+
+}
+
 register_nav_menus(
 	array(
 		'primary' => esc_html__('Primary menu', 'twentytwentyone'),
@@ -46,3 +94,46 @@ register_sidebar(
 		'after_title' => '</h2>',
 	)
 );
+
+add_filter( 'use_block_editor_for_post_type', '__return_false' );
+
+
+// для дополнительных опций темы
+// add_action('carbon_fields_register_fields', 'crb_attach_page_options');
+// function crb_attach_page_options()
+// {
+// 	Container::make('theme_options', __('Theme Options'))
+// 		->add_fields(
+// 			array(
+// 				Field::make('complex', 'slider_slider', 'Слайдер')
+// 					->add_fields(
+// 						array(
+// 							Field::make('image', 'photo', 'Slide Photo'),
+// 						)
+// 					),
+// 				Field::make('text', 'truemisha_h1', 'Заголовок'),
+// 				Field::make('textarea', 'truemisha_p', 'Текст под заголовком'),
+// 				Field::make('rich_text', 'crb_sidenote', __('Большой текст')),
+// 				Field::make('checkbox', 'crb_show_content', 'Показать доп. контент'),
+// 			)
+// 		);
+// }
+
+// отключил пока. потому все работает на carbon fields
+// add_theme_support('title-tag');
+// add_theme_support(
+// 	'post-formats',
+// 	array(
+// 		'link',
+// 		'aside',
+// 		'gallery',
+// 		'image',
+// 		'quote',
+// 		'status',
+// 		'video',
+// 		'audio',
+// 		'chat',
+// 	)
+// );
+// add_theme_support('post-thumbnails');
+// set_post_thumbnail_size(1568, 9999);
--- wp-content/themes/mall/index.php
+++ wp-content/themes/mall/index.php
@@ -1,4 +1,28 @@
 <?php get_header(); ?>
+
+<!-- vars -->
+<?
+// первый экран
+$main_slider = carbon_get_post_meta(get_the_ID(), 'main_slider');
+$title = get_post_meta(get_the_ID(), '_first_screen_title', true);
+$first_screen_text = get_post_meta(get_the_ID(), '_first_screen_text', true);
+$first_screen_title_2 = get_post_meta(get_the_ID(), '_first_screen_title_2', true);
+$first_screen_rich_text = get_post_meta(get_the_ID(), '_first_screen_rich_text', true);
+$first_screen_text_2 = get_post_meta(get_the_ID(), '_first_screen_text_2', true);
+// блок клиенты
+$clients_title = get_post_meta(get_the_ID(), '_clients_title', true);
+$slider_clients = carbon_get_post_meta(get_the_ID(), 'slider_clients');
+$clients_rich_text = get_post_meta(get_the_ID(), '_clients_rich_text', true);
+// блок баннеры
+$numbers = carbon_get_post_meta(get_the_ID(), 'numbers');
+$banner_1 = get_post_meta(get_the_ID(), '_banner_1', true);
+$banner_2 = get_post_meta(get_the_ID(), '_banner_2', true);
+// блок галерея
+$banner_3 = get_post_meta(get_the_ID(), '_banner_3', true);
+$banner_4 = get_post_meta(get_the_ID(), '_banner_4', true);
+$banner_5 = get_post_meta(get_the_ID(), '_banner_5', true);
+?>
+
 <main>
   <div class="breadcrumbs">
     Главная > Аренда
@@ -7,69 +31,54 @@
     <div class="banner">
       <div class="swiper main-banner">
         <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <img src="<? bloginfo('template_url') ?>/img/main-banner-1.webp" alt="banner">
-          </div>
-          <div class="swiper-slide">
-            <img src="<? bloginfo('template_url') ?>/img/main-banner-2.webp" alt="banner">
-          </div>
-          <div class="swiper-slide">
-            <img src="<? bloginfo('template_url') ?>/img/main-banner-3.webp" alt="banner">
-          </div>
+          <? foreach ($main_slider as $slide): ?>
+            <div class="swiper-slide">
+              <img src="<?php echo wp_get_attachment_image_url($slide['slider_photo'], 'full'); ?>" alt="banner">
+            </div>
+          <? endforeach ?>
         </div>
         <div class="swiper-pagination"></div>
       </div>
     </div>
     <div class="content">
-      <h1>Аренда в ТУЦ “Сарафан”</h1>
-      <p>Торговый центр «Сарафан», расположенный по адресу: г. Тула, ул. Путейская, д.5 (Площадь Московского вокзала),
-        предлагает помещения в аренду от собственника от 25 до 1000 кв. м.</p>
+      <h1>
+        <? echo $title; ?>
+      </h1>
+      <p>
+        <? echo $first_screen_text; ?>
+      </p>
       <p class="download">
         Скачать презентацию
         <svg width="32" height="32" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M16 21.567l-6.433-6.434 1.434-1.433 4 4V5.333h2V17.7l4-4 1.433 1.433-6.433 6.434zm-8.666 5.1c-.533 0-1-.2-1.4-.6-.4-.4-.6-.867-.6-1.4V19.9h2v4.767h17.333V19.9h2v4.767c0 .533-.2 1-.6 1.4-.4.4-.866.6-1.4.6H7.334z" fill="#151516" />
+          <path
+            d="M16 21.567l-6.433-6.434 1.434-1.433 4 4V5.333h2V17.7l4-4 1.433 1.433-6.433 6.434zm-8.666 5.1c-.533 0-1-.2-1.4-.6-.4-.4-.6-.867-.6-1.4V19.9h2v4.767h17.333V19.9h2v4.767c0 .533-.2 1-.6 1.4-.4.4-.866.6-1.4.6H7.334z"
+            fill="#151516" />
         </svg>
       </p>
-      <h2>Преимущества аренды в нашем торговом центре:</h2>
+      <h2>
+        <? echo $first_screen_title_2; ?>
+      </h2>
       <ul class="red-circle">
-        <li>ТЦ «Сарафан» находится в 5 минутах езды от центра города на площади Московского вокзала на одной из самых
-          оживленных улиц города – Красноармейский проспект.</li>
-        <li>Рядом жилой массив, пересечение основных транспортных и пешеходных потоков –конечная остановка
-          общественного транспорта, железнодорожный вокзал.</li>
-        <li>На территории ТЦ «Сарафан» есть бесплатная парковка для клиентов, более 500 машиномест, удобный подъезд.
-        </li>
-        <li>Ежедневно наш ТЦ посещает порядка 10 000 тысяч человек различной целевой аудитории</li>
-        <li>Демократичные цены за аренду площади</li>
-        <li>У нас Вы можете арендовать площадь под магазины белья, одежды и обуви, салоны, точки общепита и др. </li>
+        <? echo $first_screen_rich_text; ?>
       </ul>
-      <p>Открытие торговой точки в одном из оживленных мест города обеспечит доходность, развитие и процветание Вашего
-        бизнеса.</p>
+      <p>
+        <? echo $first_screen_text_2; ?>
+      </p>
     </div>
   </section>
   <section class="clients">
-    <h1>Более 130 бутиков и магазинов уже арендуют у нас помещения</h1>
+    <h1>
+      <? echo $clients_title; ?>
+    </h1>
     <div class="clients-section-content flex">
       <div class="clients-slider">
         <div class="swiper clients-swiper">
           <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <img src="<? bloginfo('template_url') ?>/img/partners-1.png" alt="banner">
-            </div>
-            <div class="swiper-slide">
-              <img src="<? bloginfo('template_url') ?>/img/partners-2.png" alt="banner">
-            </div>
-            <div class="swiper-slide">
-              <img src="<? bloginfo('template_url') ?>/img/partners-3.png" alt="banner">
-            </div>
-            <div class="swiper-slide">
-              <img src="<? bloginfo('template_url') ?>/img/partners-1.png" alt="banner">
-            </div>
-            <div class="swiper-slide">
-              <img src="<? bloginfo('template_url') ?>/img/partners-2.png" alt="banner">
-            </div>
-            <div class="swiper-slide">
-              <img src="<? bloginfo('template_url') ?>/img/partners-3.png" alt="banner">
-            </div>
+            <? foreach ($slider_clients as $slide): ?>
+              <div class="swiper-slide">
+                <img src="<? echo wp_get_attachment_image_url($slide['slider_photo'], 'full'); ?>" alt="banner">
+              </div>
+            <? endforeach ?>
           </div>
           <div class="control">
             <div class="swiper-button-prev"></div>
@@ -79,48 +88,32 @@
         </div>
       </div>
       <div class="content">
-        <ul class="red-circle">
-          <li>Якорные арендаторы - Лента, Детский Мир, Технопарк, Модис, Familia, Читай город, Тутанхамон, Л'Этуаль
-          </li>
-          <li>Бренды - EWA, Artigli, Gerzedo, Goergo, Du monde, Petek, Neri Karra, Ledy Collection, Paolo Conte,
-            P'Cont, VINZER, Ace, Tonnelli, Milavitsa, и др.</li>
-          <li>Магазины спортивной одежды и инвентаря: Адреналин, Спорт Лайн</li>
-          <li>Зона фудкорта - Burger King, Империя вкуса, Про Кофий, Добрая сдоба</li>
-          <li>Салоны связи: Билайн, Мегафон, YOTA, Связной, Теле2</li>
-          <li>Зоосад, аптека «Здесь аптека», «Арт-оптика», Fresh Оптика, ковры, салоны штор, текстиль Missis Hatson и
-            Home TEX, салон Арт-Самовар, посуда, подарки, картины, сувениры и другие.</li>
-        </ul>
+        <? echo $clients_rich_text ?>
       </div>
     </div>
   </section>
   <section class="advantages">
     <div class="numbers">
-      <div class="number-item">
-        <p>Общая площадь торгового центра</p>
-        <p class="number">25 816 м2</p>
-      </div>
-      <div class="number-item">
-        <p>Площадь, сдаваемая в аренду</p>
-        <p class="number">16 675 м2</p>
-      </div>
-      <div class="number-item">
-        <p>Парковка (машиномест)</p>
-        <p class="number">500</p>
-      </div>
-      <div class="number-item">
-        <p>Посещаемость (в неделю)</p>
-        <p class="number">100 000 чел</p>
-      </div>
+      <? foreach ($numbers as $number): ?>
+        <div class="number-item">
+          <p>
+            <? echo $number['numbers_text'] ?>
+          </p>
+          <p class="number">
+            <? echo $number['numbers_number'] ?>
+          </p>
+        </div>
+      <? endforeach ?>
     </div>
     <div class="banners">
-      <div class="banner-1"><img src="<? bloginfo('template_url') ?>/img/banner-1.png" alt="banner"></div>
-      <div class="banner-2"><img src="<? bloginfo('template_url') ?>/img/banner-2.png" alt="banner"></div>
+      <div class="banner-1"><img src="<?php echo wp_get_attachment_image_url($banner_1, 'full'); ?>" alt="banner"></div>
+      <div class="banner-2"><img src="<?php echo wp_get_attachment_image_url($banner_2, 'full'); ?>" alt="banner"></div>
     </div>
   </section>
   <section class="gallery">
-    <div class="banner-3"><img src="<? bloginfo('template_url') ?>/img/banner-3.png" alt="banner"></div>
-    <div class="banner-4"><img src="<? bloginfo('template_url') ?>/img/banner-4.png" alt="banner"></div>
-    <div class="banner-5"><img src="<? bloginfo('template_url') ?>/img/banner-5.png" alt="banner"></div>
+    <div class="banner-3"><img src="<?php echo wp_get_attachment_image_url($banner_3, 'full'); ?>" alt="banner"></div>
+    <div class="banner-4"><img src="<?php echo wp_get_attachment_image_url($banner_4, 'full'); ?>" alt="banner"></div>
+    <div class="banner-5"><img src="<?php echo wp_get_attachment_image_url($banner_5, 'full'); ?>" alt="banner"></div>
   </section>
 </main>
 <? get_footer(); ?>
